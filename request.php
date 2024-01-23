2d4ad0a56ac07b4d94ce8cbe99e94c7e9d615ff6
gds90 <you@example.com>
2024-01-23 14:57:20 +0100

    Added request.php file, who manage the censorship of the entered paragraph

---
+++ request.php
@@ -0,0 +1,34 @@
+<?php
+// assegno i valori dei campi input alle variabili
+$paragrafo = $_GET['paragrafo'];
+$parola = $_GET['parola_censurata'];
+
+// sostituisco le parole da censurare con gli asterischi
+$nuovo_paragrafo = str_replace($parola, '***', $paragrafo);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- mostro il paragrafo senza censurare le parole  -->
+    <h1>With Badwords</h1>
+    <div>
+        <?php echo $paragrafo; ?>
+        <p>Lunghezza del paragrafo: <?php echo strlen($paragrafo) ?></p>
+    </div>
+    <!-- mostro il paragrafo con le parole censurate -->
+    <h1>Without Badwords</h1>
+    <div>
+        <?php echo $nuovo_paragrafo; ?>
+        <p>Lunghezza del paragrafo: <?php echo strlen($nuovo_paragrafo) ?></p>
+    </div>
+</body>
+
+</html>
